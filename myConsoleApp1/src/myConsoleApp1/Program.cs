76e6cb3ca5fa77d0a16bd34061c1c5be0f673ef5
Mirko Atanasov <mirko.atanasov@gmail.com>
2017-02-03 21:50:57 -0500

    my first .net core console app

---
+++ myConsoleApp1/src/myConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("aj, za arno, za ubo!");
+            Console.ReadLine();
+        }
+    }
+}
